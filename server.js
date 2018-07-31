602fa8145dccc510335938fb9dc65ccdeae8f825
Nino Ross Rodriguez <ninoross.rodriguez@adelphi.digital>
2018-08-01 09:00:17 +1000

    - generates pattern

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const request = require('request').defaults({ encoding: null });
 const Canvas = require('canvas-prebuilt');
@@ -23,12 +24,6 @@ app.use(function (req, res, next) {
 
 
 app.get('/getqrcode', function (req, res) {
-    // const imageURL = req.body.endpoint;
-    // const imageURL = 'https://chart.googleapis.com/chart?chs=200x200&cht=qr&chl=https://www.infiniteimaginations.co/?qr=1/#/hello/';
-    /* const imageURL = 'https://chart.googleapis.com/chart?chs=200x200&cht=qr&chl=https://qfrankp.au1.qualtrics.com/jfe/form/SV_9pGqjlKZMuZetJX?BoxLabel=Nino%27s%20Box%20of%20Tricks'; */
-
-    console.log(req.query.url)
-
     const imageURL = 'https://chart.googleapis.com/chart?chs=200x200&cht=qr&chl=' + req.query.url;
 
     request({
@@ -86,6 +81,8 @@ app.get('/getqrcode', function (req, res) {
         };
         innerImage.src = dataURI;
 
+        encodeImage(dataURI);
+
         let base64Img = canvasDataURL.split(",")[1];
         let fullMarkerImage = new Buffer(base64Img, 'base64');
 
@@ -102,3 +99,61 @@ app.get('/getqrcode', function (req, res) {
 app.listen(process.env.PORT || 8888, function () {
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
+
+function encodeImage(dataURI) {
+    console.log('encodeImage');
+    var image = new Image;
+    image.onload = function () {
+        console.log('Encoded Image loaded');
+        // copy image on canvas
+        var canvas = new Canvas;
+        var context = canvas.getContext('2d')
+        canvas.width = 16;
+        canvas.height = 16;
+
+        // document.body.appendChild(canvas)
+        // canvas.style.width = '200px'
+
+
+        var patternFileString = ''
+        for (var orientation = 0; orientation > -2 * Math.PI; orientation -= Math.PI / 2) {
+            // draw on canvas - honor orientation
+            context.save();
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            context.translate(canvas.width / 2, canvas.height / 2);
+            context.rotate(orientation);
+            context.drawImage(image, -canvas.width / 2, -canvas.height / 2, canvas.width, canvas.height);
+            context.restore();
+
+            // get imageData
+            var imageData = context.getImageData(0, 0, canvas.width, canvas.height)
+
+            // generate the patternFileString for this orientation
+            if (orientation !== 0) patternFileString += '\n'
+            // NOTE bgr order and not rgb!!! so from 2 to 0
+            for (var channelOffset = 2; channelOffset >= 0; channelOffset--) {
+                // console.log('channelOffset', channelOffset)
+                for (var y = 0; y < imageData.height; y++) {
+                    for (var x = 0; x < imageData.width; x++) {
+
+                        if (x !== 0) patternFileString += ' '
+
+                        var offset = (y * imageData.width * 4) + (x * 4) + channelOffset
+                        var value = imageData.data[offset]
+
+                        patternFileString += String(value).padStart(3);
+                    }
+                    patternFileString += '\n'
+                }
+            }
+        }
+
+        fs.writeFile('./pattern/pattern-marker.patt', patternFileString, (err) => {
+            if (err) {
+                throw err;
+            }
+            console.log('The pattern has been generated!');
+        });
+    }
+    image.src = dataURI;
+};
